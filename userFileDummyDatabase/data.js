3bc2e508aacae89ac0e6c75543d60aebbcac7ea7
oyamasiphula <oyama@projectcodex.co>
2017-06-17 02:05:19 +0200

    added the gh repository content API data

---
+++ userFileDummyDatabase/data.js
@@ -0,0 +1,257 @@
+module.exports = [{
+        "name": ".gitignore",
+        "path": ".gitignore",
+        "sha": "8c30278a7a1a13a6fc7fb49b52c56aee58cc8ff9",
+        "size": 25,
+        "url": "https://api.github.com/repos/Oyamasiphula/bootCamp_workouts_CS/contents/.gitignore?ref=master",
+        "html_url": "https://github.com/Oyamasiphula/bootCamp_workouts_CS/blob/master/.gitignore",
+        "git_url": "https://api.github.com/repos/Oyamasiphula/bootCamp_workouts_CS/git/blobs/8c30278a7a1a13a6fc7fb49b52c56aee58cc8ff9",
+        "download_url": "https://raw.githubusercontent.com/Oyamasiphula/bootCamp_workouts_CS/master/.gitignore",
+        "type": "file",
+        "_links": {
+            "self": "https://api.github.com/repos/Oyamasiphula/bootCamp_workouts_CS/contents/.gitignore?ref=master",
+            "git": "https://api.github.com/repos/Oyamasiphula/bootCamp_workouts_CS/git/blobs/8c30278a7a1a13a6fc7fb49b52c56aee58cc8ff9",
+            "html": "https://github.com/Oyamasiphula/bootCamp_workouts_CS/blob/master/.gitignore"
+        }
+    },
+    {
+        "name": "CS_exercises",
+        "path": "CS_exercises",
+        "sha": "606179069792528cbf2f0c5b25b2142617e29806",
+        "size": 0,
+        "url": "https://api.github.com/repos/Oyamasiphula/bootCamp_workouts_CS/contents/CS_exercises?ref=master",
+        "html_url": "https://github.com/Oyamasiphula/bootCamp_workouts_CS/tree/master/CS_exercises",
+        "git_url": "https://api.github.com/repos/Oyamasiphula/bootCamp_workouts_CS/git/trees/606179069792528cbf2f0c5b25b2142617e29806",
+        "download_url": null,
+        "type": "dir",
+        "_links": {
+            "self": "https://api.github.com/repos/Oyamasiphula/bootCamp_workouts_CS/contents/CS_exercises?ref=master",
+            "git": "https://api.github.com/repos/Oyamasiphula/bootCamp_workouts_CS/git/trees/606179069792528cbf2f0c5b25b2142617e29806",
+            "html": "https://github.com/Oyamasiphula/bootCamp_workouts_CS/tree/master/CS_exercises"
+        }
+    },
+    {
+        "name": "Model",
+        "path": "Model",
+        "sha": "a93abfe6d8f019d6c6a5e357191386b718df2c97",
+        "size": 0,
+        "url": "https://api.github.com/repos/Oyamasiphula/bootCamp_workouts_CS/contents/Model?ref=master",
+        "html_url": "https://github.com/Oyamasiphula/bootCamp_workouts_CS/tree/master/Model",
+        "git_url": "https://api.github.com/repos/Oyamasiphula/bootCamp_workouts_CS/git/trees/a93abfe6d8f019d6c6a5e357191386b718df2c97",
+        "download_url": null,
+        "type": "dir",
+        "_links": {
+            "self": "https://api.github.com/repos/Oyamasiphula/bootCamp_workouts_CS/contents/Model?ref=master",
+            "git": "https://api.github.com/repos/Oyamasiphula/bootCamp_workouts_CS/git/trees/a93abfe6d8f019d6c6a5e357191386b718df2c97",
+            "html": "https://github.com/Oyamasiphula/bootCamp_workouts_CS/tree/master/Model"
+        }
+    },
+    {
+        "name": "README.md",
+        "path": "README.md",
+        "sha": "ea418abd212286b856603ade9cfca0922ad33957",
+        "size": 23,
+        "url": "https://api.github.com/repos/Oyamasiphula/bootCamp_workouts_CS/contents/README.md?ref=master",
+        "html_url": "https://github.com/Oyamasiphula/bootCamp_workouts_CS/blob/master/README.md",
+        "git_url": "https://api.github.com/repos/Oyamasiphula/bootCamp_workouts_CS/git/blobs/ea418abd212286b856603ade9cfca0922ad33957",
+        "download_url": "https://raw.githubusercontent.com/Oyamasiphula/bootCamp_workouts_CS/master/README.md",
+        "type": "file",
+        "_links": {
+            "self": "https://api.github.com/repos/Oyamasiphula/bootCamp_workouts_CS/contents/README.md?ref=master",
+            "git": "https://api.github.com/repos/Oyamasiphula/bootCamp_workouts_CS/git/blobs/ea418abd212286b856603ade9cfca0922ad33957",
+            "html": "https://github.com/Oyamasiphula/bootCamp_workouts_CS/blob/master/README.md"
+        }
+    },
+    {
+        "name": "addThem.js",
+        "path": "addThem.js",
+        "sha": "6599429c4c5d46cf32a72ac9020acfcd5330c8c1",
+        "size": 1305,
+        "url": "https://api.github.com/repos/Oyamasiphula/bootCamp_workouts_CS/contents/addThem.js?ref=master",
+        "html_url": "https://github.com/Oyamasiphula/bootCamp_workouts_CS/blob/master/addThem.js",
+        "git_url": "https://api.github.com/repos/Oyamasiphula/bootCamp_workouts_CS/git/blobs/6599429c4c5d46cf32a72ac9020acfcd5330c8c1",
+        "download_url": "https://raw.githubusercontent.com/Oyamasiphula/bootCamp_workouts_CS/master/addThem.js",
+        "type": "file",
+        "_links": {
+            "self": "https://api.github.com/repos/Oyamasiphula/bootCamp_workouts_CS/contents/addThem.js?ref=master",
+            "git": "https://api.github.com/repos/Oyamasiphula/bootCamp_workouts_CS/git/blobs/6599429c4c5d46cf32a72ac9020acfcd5330c8c1",
+            "html": "https://github.com/Oyamasiphula/bootCamp_workouts_CS/blob/master/addThem.js"
+        }
+    },
+    {
+        "name": "couternTest.md",
+        "path": "couternTest.md",
+        "sha": "e71985c3f6205c885189a4f14c3b650de1b2e594",
+        "size": 56,
+        "url": "https://api.github.com/repos/Oyamasiphula/bootCamp_workouts_CS/contents/couternTest.md?ref=master",
+        "html_url": "https://github.com/Oyamasiphula/bootCamp_workouts_CS/blob/master/couternTest.md",
+        "git_url": "https://api.github.com/repos/Oyamasiphula/bootCamp_workouts_CS/git/blobs/e71985c3f6205c885189a4f14c3b650de1b2e594",
+        "download_url": "https://raw.githubusercontent.com/Oyamasiphula/bootCamp_workouts_CS/master/couternTest.md",
+        "type": "file",
+        "_links": {
+            "self": "https://api.github.com/repos/Oyamasiphula/bootCamp_workouts_CS/contents/couternTest.md?ref=master",
+            "git": "https://api.github.com/repos/Oyamasiphula/bootCamp_workouts_CS/git/blobs/e71985c3f6205c885189a4f14c3b650de1b2e594",
+            "html": "https://github.com/Oyamasiphula/bootCamp_workouts_CS/blob/master/couternTest.md"
+        }
+    },
+    {
+        "name": "css",
+        "path": "css",
+        "sha": "d96d9d8a55a38f7637d42b69afeecd89e4f03ad2",
+        "size": 0,
+        "url": "https://api.github.com/repos/Oyamasiphula/bootCamp_workouts_CS/contents/css?ref=master",
+        "html_url": "https://github.com/Oyamasiphula/bootCamp_workouts_CS/tree/master/css",
+        "git_url": "https://api.github.com/repos/Oyamasiphula/bootCamp_workouts_CS/git/trees/d96d9d8a55a38f7637d42b69afeecd89e4f03ad2",
+        "download_url": null,
+        "type": "dir",
+        "_links": {
+            "self": "https://api.github.com/repos/Oyamasiphula/bootCamp_workouts_CS/contents/css?ref=master",
+            "git": "https://api.github.com/repos/Oyamasiphula/bootCamp_workouts_CS/git/trees/d96d9d8a55a38f7637d42b69afeecd89e4f03ad2",
+            "html": "https://github.com/Oyamasiphula/bootCamp_workouts_CS/tree/master/css"
+        }
+    },
+    {
+        "name": "fonts",
+        "path": "fonts",
+        "sha": "fd865ec4ce5d0161aa1800bc9af3053752818f54",
+        "size": 0,
+        "url": "https://api.github.com/repos/Oyamasiphula/bootCamp_workouts_CS/contents/fonts?ref=master",
+        "html_url": "https://github.com/Oyamasiphula/bootCamp_workouts_CS/tree/master/fonts",
+        "git_url": "https://api.github.com/repos/Oyamasiphula/bootCamp_workouts_CS/git/trees/fd865ec4ce5d0161aa1800bc9af3053752818f54",
+        "download_url": null,
+        "type": "dir",
+        "_links": {
+            "self": "https://api.github.com/repos/Oyamasiphula/bootCamp_workouts_CS/contents/fonts?ref=master",
+            "git": "https://api.github.com/repos/Oyamasiphula/bootCamp_workouts_CS/git/trees/fd865ec4ce5d0161aa1800bc9af3053752818f54",
+            "html": "https://github.com/Oyamasiphula/bootCamp_workouts_CS/tree/master/fonts"
+        }
+    },
+    {
+        "name": "images",
+        "path": "images",
+        "sha": "4d2cef061e0d99aaa7af72e5b1e55b9ef3f9394b",
+        "size": 0,
+        "url": "https://api.github.com/repos/Oyamasiphula/bootCamp_workouts_CS/contents/images?ref=master",
+        "html_url": "https://github.com/Oyamasiphula/bootCamp_workouts_CS/tree/master/images",
+        "git_url": "https://api.github.com/repos/Oyamasiphula/bootCamp_workouts_CS/git/trees/4d2cef061e0d99aaa7af72e5b1e55b9ef3f9394b",
+        "download_url": null,
+        "type": "dir",
+        "_links": {
+            "self": "https://api.github.com/repos/Oyamasiphula/bootCamp_workouts_CS/contents/images?ref=master",
+            "git": "https://api.github.com/repos/Oyamasiphula/bootCamp_workouts_CS/git/trees/4d2cef061e0d99aaa7af72e5b1e55b9ef3f9394b",
+            "html": "https://github.com/Oyamasiphula/bootCamp_workouts_CS/tree/master/images"
+        }
+    },
+    {
+        "name": "index.html",
+        "path": "index.html",
+        "sha": "bda4f4eabe437c4db1b3bd7b1f038e2bee366fd1",
+        "size": 1845,
+        "url": "https://api.github.com/repos/Oyamasiphula/bootCamp_workouts_CS/contents/index.html?ref=master",
+        "html_url": "https://github.com/Oyamasiphula/bootCamp_workouts_CS/blob/master/index.html",
+        "git_url": "https://api.github.com/repos/Oyamasiphula/bootCamp_workouts_CS/git/blobs/bda4f4eabe437c4db1b3bd7b1f038e2bee366fd1",
+        "download_url": "https://raw.githubusercontent.com/Oyamasiphula/bootCamp_workouts_CS/master/index.html",
+        "type": "file",
+        "_links": {
+            "self": "https://api.github.com/repos/Oyamasiphula/bootCamp_workouts_CS/contents/index.html?ref=master",
+            "git": "https://api.github.com/repos/Oyamasiphula/bootCamp_workouts_CS/git/blobs/bda4f4eabe437c4db1b3bd7b1f038e2bee366fd1",
+            "html": "https://github.com/Oyamasiphula/bootCamp_workouts_CS/blob/master/index.html"
+        }
+    },
+    {
+        "name": "jquery-3.1.1.min.js",
+        "path": "jquery-3.1.1.min.js",
+        "sha": "4c5be4c0fbe230e81d95718a18829e965a2d14b2",
+        "size": 86709,
+        "url": "https://api.github.com/repos/Oyamasiphula/bootCamp_workouts_CS/contents/jquery-3.1.1.min.js?ref=master",
+        "html_url": "https://github.com/Oyamasiphula/bootCamp_workouts_CS/blob/master/jquery-3.1.1.min.js",
+        "git_url": "https://api.github.com/repos/Oyamasiphula/bootCamp_workouts_CS/git/blobs/4c5be4c0fbe230e81d95718a18829e965a2d14b2",
+        "download_url": "https://raw.githubusercontent.com/Oyamasiphula/bootCamp_workouts_CS/master/jquery-3.1.1.min.js",
+        "type": "file",
+        "_links": {
+            "self": "https://api.github.com/repos/Oyamasiphula/bootCamp_workouts_CS/contents/jquery-3.1.1.min.js?ref=master",
+            "git": "https://api.github.com/repos/Oyamasiphula/bootCamp_workouts_CS/git/blobs/4c5be4c0fbe230e81d95718a18829e965a2d14b2",
+            "html": "https://github.com/Oyamasiphula/bootCamp_workouts_CS/blob/master/jquery-3.1.1.min.js"
+        }
+    },
+    {
+        "name": "js",
+        "path": "js",
+        "sha": "148d5a1980eeea6c577a78d78e3213ac3a05ac0f",
+        "size": 0,
+        "url": "https://api.github.com/repos/Oyamasiphula/bootCamp_workouts_CS/contents/js?ref=master",
+        "html_url": "https://github.com/Oyamasiphula/bootCamp_workouts_CS/tree/master/js",
+        "git_url": "https://api.github.com/repos/Oyamasiphula/bootCamp_workouts_CS/git/trees/148d5a1980eeea6c577a78d78e3213ac3a05ac0f",
+        "download_url": null,
+        "type": "dir",
+        "_links": {
+            "self": "https://api.github.com/repos/Oyamasiphula/bootCamp_workouts_CS/contents/js?ref=master",
+            "git": "https://api.github.com/repos/Oyamasiphula/bootCamp_workouts_CS/git/trees/148d5a1980eeea6c577a78d78e3213ac3a05ac0f",
+            "html": "https://github.com/Oyamasiphula/bootCamp_workouts_CS/tree/master/js"
+        }
+    },
+    {
+        "name": "less",
+        "path": "less",
+        "sha": "bfc1b4432f0cb993ad81dfdb507136077b93178a",
+        "size": 0,
+        "url": "https://api.github.com/repos/Oyamasiphula/bootCamp_workouts_CS/contents/less?ref=master",
+        "html_url": "https://github.com/Oyamasiphula/bootCamp_workouts_CS/tree/master/less",
+        "git_url": "https://api.github.com/repos/Oyamasiphula/bootCamp_workouts_CS/git/trees/bfc1b4432f0cb993ad81dfdb507136077b93178a",
+        "download_url": null,
+        "type": "dir",
+        "_links": {
+            "self": "https://api.github.com/repos/Oyamasiphula/bootCamp_workouts_CS/contents/less?ref=master",
+            "git": "https://api.github.com/repos/Oyamasiphula/bootCamp_workouts_CS/git/trees/bfc1b4432f0cb993ad81dfdb507136077b93178a",
+            "html": "https://github.com/Oyamasiphula/bootCamp_workouts_CS/tree/master/less"
+        }
+    },
+    {
+        "name": "lib",
+        "path": "lib",
+        "sha": "8fd5dc26ca635796317d9542e5ed0191ce811197",
+        "size": 0,
+        "url": "https://api.github.com/repos/Oyamasiphula/bootCamp_workouts_CS/contents/lib?ref=master",
+        "html_url": "https://github.com/Oyamasiphula/bootCamp_workouts_CS/tree/master/lib",
+        "git_url": "https://api.github.com/repos/Oyamasiphula/bootCamp_workouts_CS/git/trees/8fd5dc26ca635796317d9542e5ed0191ce811197",
+        "download_url": null,
+        "type": "dir",
+        "_links": {
+            "self": "https://api.github.com/repos/Oyamasiphula/bootCamp_workouts_CS/contents/lib?ref=master",
+            "git": "https://api.github.com/repos/Oyamasiphula/bootCamp_workouts_CS/git/trees/8fd5dc26ca635796317d9542e5ed0191ce811197",
+            "html": "https://github.com/Oyamasiphula/bootCamp_workouts_CS/tree/master/lib"
+        }
+    },
+    {
+        "name": "scss",
+        "path": "scss",
+        "sha": "721acdb78e26cf746a5d80970e7d8e621a9189bc",
+        "size": 0,
+        "url": "https://api.github.com/repos/Oyamasiphula/bootCamp_workouts_CS/contents/scss?ref=master",
+        "html_url": "https://github.com/Oyamasiphula/bootCamp_workouts_CS/tree/master/scss",
+        "git_url": "https://api.github.com/repos/Oyamasiphula/bootCamp_workouts_CS/git/trees/721acdb78e26cf746a5d80970e7d8e621a9189bc",
+        "download_url": null,
+        "type": "dir",
+        "_links": {
+            "self": "https://api.github.com/repos/Oyamasiphula/bootCamp_workouts_CS/contents/scss?ref=master",
+            "git": "https://api.github.com/repos/Oyamasiphula/bootCamp_workouts_CS/git/trees/721acdb78e26cf746a5d80970e7d8e621a9189bc",
+            "html": "https://github.com/Oyamasiphula/bootCamp_workouts_CS/tree/master/scss"
+        }
+    },
+    {
+        "name": "test",
+        "path": "test",
+        "sha": "e6147569983a5a52b6b6343bdb75ec42c2cc7f4d",
+        "size": 0,
+        "url": "https://api.github.com/repos/Oyamasiphula/bootCamp_workouts_CS/contents/test?ref=master",
+        "html_url": "https://github.com/Oyamasiphula/bootCamp_workouts_CS/tree/master/test",
+        "git_url": "https://api.github.com/repos/Oyamasiphula/bootCamp_workouts_CS/git/trees/e6147569983a5a52b6b6343bdb75ec42c2cc7f4d",
+        "download_url": null,
+        "type": "dir",
+        "_links": {
+            "self": "https://api.github.com/repos/Oyamasiphula/bootCamp_workouts_CS/contents/test?ref=master",
+            "git": "https://api.github.com/repos/Oyamasiphula/bootCamp_workouts_CS/git/trees/e6147569983a5a52b6b6343bdb75ec42c2cc7f4d",
+            "html": "https://github.com/Oyamasiphula/bootCamp_workouts_CS/tree/master/test"
+        }
+    }
+]
